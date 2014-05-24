42e6d09df4ba8df2baf49ece66e26b4058d10e75
Ed Swindelles <ed@swindelles.com>
2014-05-23 23:47:32 -0400

    Documentation
    Personalized README, moved example usage there from the R file.

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,5 @@
 # Functions to compute the inverse of a matrix with caching.
 
-# Example usage:
-
-# > m <- matrix(rnorm(25), 5)     # Create a 5x5 matrix of random numbers
-# > cache <- makeCacheMatrix(m)   # Create the cache list
-# > cache$get()                   # Returns the original matrix
-# > cacheSolve(cache)             # Computes the matrix inverse
-# > cacheSolve(cache)             # Again, but from cache.  Will see output
-#                                 #    "getting cached data"
-
-# Demonstrate caching performance:
-
-# > x <- 2000
-# > m <- matrix(rnorm(x^2), x)
-# > c <- makeCacheMatrix(m)
-# > t <- system.time(cacheSolve(c))
-# > t
-#    user  system elapsed 
-#  23.930   0.073  24.122 
-# > t <- system.time(cacheSolve(c))
-# getting cached data
-# > t
-#    user  system elapsed 
-#       0       0       0 
-
 
 # Caching function for a matrix argument.  Returns a vector 
 # of getters and setters for the matrix itself, as well as 
